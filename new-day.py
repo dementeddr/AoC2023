5e4445a609bc18e050e011fc76b0debae243e5ab
dementeddr <2413901+dementeddr@users.noreply.github.com>
2023-12-05 11:30:09 -0500

    Made new-day script create blank input files

--- new-day.py
+++ new-day.py
@@ -14,6 +14,8 @@ else:
     print("Please enter a day number")
     exit()
 
+
+
 print("Reading env variables")
 load_dotenv()
 year = os.getenv("YEAR")
@@ -21,8 +23,11 @@ monster = {"session" : os.getenv("COOKIE")}
 
 dir_name = f"{day}day"
 
+
 print(f"Making directory: \"{dir_name}\"")
 os.mkdir(dir_name)
+
+
 
 print("Copying template file")
 template = ''
@@ -37,18 +42,38 @@ with open(f"{dir_name}/{day}-1.py", "w") as p_fp:
 
 os.chmod(f"{dir_name}/{day}-1.py", 0o777)
 
-print(f"Requesting input file for {year}, day {day}")
-input_file = requests.get(f"https://adventofcode.com/{year}/day/{day}/input", cookies=monster)
-print(f"Response code: {input_file.status_code}")
 
-if input_file.status_code != 200:
-    con = input("Cannot access input file. Continue? [y/N]: ")
-    if con not in ('Y', 'y'):
-        exit()
+
+print("Writing blank test input file")
+with open(f"{dir_name}/test-input.txt", "w") as in_fp:
+    in_fp.write("")
+
+
+
+print(f"Requesting input file for {year}, day {day}")
+print_blank = False
+
+try:
+    input_file = requests.get(f"https://adventofcode.com/{year}/day/{day}/input", cookies=monster)
+    print(f"Response code: {input_file.status_code}")
+
+    if input_file.status_code != 200:
+        con = input("Cannot retrieve input file.")
+        print_blank = True
+        
+    else:
+        print("Writing input file")
+        with open(f"{dir_name}/input-d{day}.txt", "w") as in_fp:
+            in_fp.write(input_file.text)
+except:
+    print("Exception while trying to create input file.")
+    print_blank = True
     
-else:
-    print("Writing input file")
+if print_blank:
+    print("Creating blank file instead.")
     with open(f"{dir_name}/input-d{day}.txt", "w") as in_fp:
-        in_fp.write(input_file.text)
+        in_fp.write("")
+
+
 
 print("Done")
