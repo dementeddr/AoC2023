902773d32184cf209cf57960bd5f5e0833146740
dementeddr <2413901+dementeddr@users.noreply.github.com>
2023-12-07 09:05:25 -0500

    Day 6 all done. Brute force is best force

--- 6day/6-1.py
+++ 6day/6-1.py
@@ -10,9 +10,27 @@ _day = 6 #Advent of Code day
 
 def main(data):
 
+    num_pat = re.compile(r"(\d+)")
+    times = list(map(int,num_pat.findall(data[0])))
+    dists = list(map(int,num_pat.findall(data[1])))
+    print(times)
+    print(dists)
+
+    beats = 1
+    for race in range(len(times)):
+        print(f"{race}: time = {times[race]}, dist = {dists[race]}")
+        rng = 0
+        for i in range(times[race]):
+            travel = i * (times[race] - i)
+            if travel > dists[race]:
+                rng += 1
+            print(f"  [{i}]: travel = {travel}, range = {rng}")
+        beats *= rng
+
+    print(f"Margin = {beats}")
 
 
-    
+
 ## END SOLUTION
 
 if __name__ == "__main__":
@@ -30,5 +48,5 @@ if __name__ == "__main__":
     time_stop = perf_counter_ns()
 
     time = (time_stop - time_start) / 1000000
-    print(f"Execution took {time:.4f} milliseconds")
+    print(f"\nExecution took {time:.4f} milliseconds")
 
--- 6day/6-2.py
+++ 6day/6-2.py
@@ -4,15 +4,28 @@ import sys
 import re
 from time import perf_counter_ns
 
-_day = $$ #Advent of Code day
+_day = 6 #Advent of Code day
 
 ## BEGIN SOLUTION
 
 def main(data):
 
+    num_pat = re.compile(r"(\d+)")
+    time = int("".join(num_pat.findall(data[0])))
+    dist = int("".join(num_pat.findall(data[1])))
+    print(time)
+    print(dist)
+
+    beats = 0
+    for i in range(time):
+        travel = i * (time - i)
+        if travel > dist:
+            beats += 1
+        #print(f"  [{i}]: travel = {travel}, beats = {beats}")
+
+    print(f"Margin = {beats}")
 
 
-    
 ## END SOLUTION
 
 if __name__ == "__main__":
